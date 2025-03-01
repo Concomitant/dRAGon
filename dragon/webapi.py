88ea8e2487e357027ad7766ff5cd6a1604fde80f
Daniel Shank <daniel.p.shank@gmail.com>
2025-03-01 12:32:53 -0500

    Feature/improved generation (#12)
    * search update to OR
    
    * improved prompt
    
    * test commit
    
    * upgraded prompt with 'sandwich' technique

--- dragon/webapi.py
+++ dragon/webapi.py
@@ -19,9 +19,23 @@ llm = OpenAI(temperature=0.7)
 rag_prompt = PromptTemplate(
     input_variables=["question", "source"],
     template=(
-        f"Answer the following question as it applies to the {rpg_title} RPG by {rpg_author}: {{question}}\n"
-        f"use these sources {{source}}. Remember to answer the question to the best of your ability using the sources."
-        f"If there is no apparent answer, reply instead that you are unable to answer the question."
+        f"""Examine the following snippets from {rpg_title} RPG. Following the snippets will be a question about the content\n
+        If the answer to the question is found in the text answer accordingly. Otherwise reply that the answer is not found 
+        in the provided text, and suggest a modified version of the question that might yield better results when provided 
+        to a search engine.
+        \n
+        {{source}}\n
+        {{question}}\n
+        \n
+        Remember to  provide answers that are found in the provided snippet from {rpg_title}. If the content concerns a different 
+        subject than the question, answer in the negative and suggest an alternative query to answer the user's question.
+        
+        Examine the above snippets from {rpg_title} RPG. Following the snippets will be a question about the content\n
+        If the answer to the question is found in the text answer accordingly. Otherwise reply that the answer is not found 
+        in the provided text, and suggest a modified version of the question that might yield better results when provided 
+        to a search engine.
+
+        Reply below:\n\n"""
     ),
 )
 
